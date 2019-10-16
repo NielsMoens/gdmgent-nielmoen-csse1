40848c45ad0850bfac51ca2be93f55b768073e8e
NielsMoens <nielmoen@student.arteveldehs.be>
2019-10-16 20:48:12 +0200

    opdracht 3

---
+++ Opdracht3/Faculteit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace oef4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FACULTEIT
+            //n -> uitlezen
+            // spast proefen van variablen 
+            // door het gebruik van long i.p.v.  
+            long n,j;
+            Console.WriteLine("=================================\n FACULTEIT\n=================================");
+            Console.Write("Input the number: ");
+            n= Convert.ToInt32(Console.ReadLine());   
+            Console.Write("\n");
+            long facul=1;
+            if(n > 0){
+                for(j=1;j<=n;j++)
+                {
+                facul= facul*j;
+                }
+                Console.WriteLine(facul);
+            }else{
+                Console.WriteLine("result = "+ facul);
+            }
+            
+        }
+    }
+}
---
+++ Opdracht3/Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace oef5
+{
+    class Program
+    {
+        static void Main(string[] args){
+            // FIBONACCI
+            // n -> lengte reeks
+            // spast proefen van variablen
+            long firstVal=0 , thrVal = 0; 
+            long secVal = 1;
+            Console.WriteLine("=================================\n DE RIJ VAN FIBONACCI\n=================================");
+            Console.Write("Input the Row number: ");
+            long loop = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("\n");
+            Console.WriteLine(firstVal);
+            Console.WriteLine(secVal);
+            for (int i = 2; i < loop; i++){
+                thrVal = firstVal + secVal;
+                firstVal = secVal;
+                secVal = thrVal;
+                Console.WriteLine(thrVal);
+            }
+        }
+    }
+}
