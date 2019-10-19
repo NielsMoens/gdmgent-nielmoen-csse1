b3d6dbd0a9130ca56ab1598c9c51241978d60d99
NielsMoens <nielmoen@student.arteveldehs.be>
2019-10-19 13:33:46 +0200

    les 4

---
+++ lessen/les4/Oefeningen/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Oefeningen
+{
+    class Program
+    {
+        static void genFac(){
+        // dit is een locale var
+            
+        }
+        static void genFib(int gen){
+        // dit is een locale var
+            Console.WriteLine("test " + lengte);
+            long firstVal=0 , thrVal = 0; 
+            long secVal = 1;
+            Console.WriteLine("=================================\n DE RIJ VAN FIBONACCI\n=================================");
+            Console.Write("Input the Row number: ");
+            long loop = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("\n");
+            Console.WriteLine(firstVal);
+            Console.WriteLine(secVal);
+            for (int i = 2; i < loop; i++){
+                thrVal = firstVal + secVal;
+                firstVal = secVal;
+                secVal = thrVal;
+                Console.WriteLine(thrVal);
+                }
+                
+        }
+        static void Main(string[] args)
+        {
+            genFac();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ lessen/les4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace les4
+{
+    class Program
+    {
+        int teller= 0; //dit is een globale var
+        static void genFib(int lengte){
+        // dit is een locale var
+            Console.WriteLine("test " + lengte);
+        }
+        static void Main(string[] args)
+        {
+            int getal1 = 8;
+            Console.WriteLine("Hello World!");
+            genFib(getal1);
+        }
+    }
+}
