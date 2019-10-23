be1306e5a626292a606d11968becf6a464f3fe16
NielsMoens <nielmoen@student.arteveldehs.be>
2019-10-23 21:02:38 +0200

    WIP

--- Opdracht4/Calculator.cs
+++ Opdracht4/Calculator.cs
@@ -4,30 +4,30 @@ namespace Opdracht4
 {
     class Calculator
     {
-        public double Number1 { get; private set; }
+        public decimal Number1 { get; private set; }
 
-        public double Number2 { get; private set; }
+        public decimal Number2 { get; private set; }
 
-        public Calculator(double number1, double number2) {
+        public Calculator(decimal number1, decimal number2) {
             this.Number1 = number1;
             this.Number2 = number2;
         }
 
-        public double add() {
+        public decimal add() {
             return this.Number1 + this.Number2;
         }
 
-        public double subtract() {
+        public decimal subtract() {
             return this.Number1 - this.Number2;
         }
         
-        public double divide() {
+        public decimal divide() {
             if (this.Number2 == 0) {
                 throw new ArgumentException("Cannot divide by zero!!!");
             }
-            return this.Number1 / this.Number2;
+            return Math.Round(this.Number1 / this.Number2, 2);
         }
-        public double multiply() {
+        public decimal multiply() {
             return this.Number1 * this.Number2;
         }
 
