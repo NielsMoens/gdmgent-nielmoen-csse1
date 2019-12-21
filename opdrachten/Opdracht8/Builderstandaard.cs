5dae7a7efad6367b679f426b975b51b5b6568f0b
NielsMoens <nielmoen@student.arteveldehs.be>
2019-12-21 14:05:26 +0100

    Opdracht8, building patterns: Metro & DeStandaard

---
+++ opdrachten/Opdracht8/Builderstandaard.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+
+namespace Opdracht8
+{
+    public class ConcreteBuilderDestandaard : IBuilder
+    {
+        private Krant _krant = new Krant();
+        
+        // A fresh builder instance should contain a blank product object, which
+        // is used in further assembly.
+        public ConcreteBuilderDestandaard()
+        {
+            this.Reset();
+        }
+        
+        
+        public void Reset()
+        {
+            this._krant = new Krant();
+        }
+        
+        // All production steps work with the same product instance.
+        public void secties()
+        {
+            this._krant.Add("Economie\n");
+            this._krant.Add("Binneland\n");
+            this._krant.Add("Buitenland\n");
+        }
+        
+        public void Artikels()
+        {
+          this._krant.Add("MediaArtikel\n");
+            this._krant.Add("NieuwsArtikel\n");
+            this._krant.Add("TechnologieArtikel\n");
+        }
+        
+        public void Reclame()
+        {
+            this._krant.Add("ad1 MediaMark -> JAWADE DADE\n");
+            this._krant.Add("ad2 Van den borre ad\n");
+            this._krant.Add("ad3 Proximus ad\n");
+        }
+        
+        // Concrete Builders are supposed to provide their own methods for
+        // retrieving results. That's because various types of builders may
+        // create entirely different products that don't follow the same
+        // interface. Therefore, such methods cannot be declared in the base
+        // Builder interface (at least in a statically typed programming
+        // language).
+        //
+        // Usually, after returning the end result to the client, a builder
+        // instance is expected to be ready to start producing another product.
+        // That's why it's a usual practice to call the reset method at the end
+        // of the `GetProduct` method body. However, this behavior is not
+        // mandatory, and you can make your builders wait for an explicit reset
+        // call from the client code before disposing of the previous result.
+        public Krant GetProduct()
+        {
+            Krant result = this._krant;
+
+            this.Reset();
+
+            return result;
+        }
+    }
+}
---
+++ opdrachten/Opdracht8/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Opdracht8
+{
+     // The Builder interface specifies methods for creating the different parts
+    // of the Krant objects.
+    public interface IBuilder
+    {
+        void secties();
+        void Artikels();
+        void Reclame();
+    }
+    
+    public class Krant
+    {
+        private List<object> _content = new List<object>();
+
+        public void Add(string part)
+        {
+            this._content.Add(part);
+        }
+        
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for (int i = 0; i < this._content.Count; i++)
+            {
+                str += this._content[i] + ", ";
+            }
+
+            str = str.Remove(str.Length - 2); // removing last ",c"
+
+            return "Krant parts: " + str + "\n";
+        }
+    }
+    
+    // The Director is only responsible for executing the building steps in a
+    // particular sequence. It is helpful when producing products according to a
+    // specific order or configuration. Strictly speaking, the Director class is
+    // optional, since the client can control builders directly.
+    public class Director
+    {
+        private IBuilder _builder;
+        
+        public IBuilder Builder
+        {
+            set { _builder = value; } 
+        }
+        
+        // The Director can construct several product variations using the same
+        // building steps.
+        public void buildMinimalViableProduct()
+        {
+            this._builder.secties();
+        }
+    
+        public void buildFullFeaturedProduct()
+        {
+            this._builder.secties();
+            this._builder.Artikels();
+            this._builder.Reclame();
+        }
+    }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code creates a builder object, passes it to the
+            // director and then initiates the construction process. The end
+            // result is retrieved from the builder object.
+            var director = new Director();
+            var builder = new ConcreteBuilderMetro();
+            director.Builder = builder;
+
+            Console.WriteLine("full featured krantMetro:");
+            director.buildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+
+            var builder2 = new ConcreteBuilderDestandaard();
+            director.Builder = builder2;
+
+            Console.WriteLine("full featured krantStandaard:");
+            director.buildFullFeaturedProduct();
+            Console.WriteLine(builder2.GetProduct().ListParts());
+
+        }
+    }
+}
